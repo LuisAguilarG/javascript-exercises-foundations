4146de5205f23824c4b845ba0a3ebfdf17adfeba
Luis Aguilar <95900211+LuisAguilarG@users.noreply.github.com>
2025-01-26 20:50:27 -0600

    Finish 08 and 09 exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,26 @@
-const add = function() {
-	
+const add = function(numberA, numberB) {
+	return numberA + numberB;
 };
 
-const subtract = function() {
-	
+const subtract = function(numberA, numberB) {
+	return numberA - numberB;
 };
 
-const sum = function() {
-	
+const sum = function(arrayNumbers) {
+
+	return arrayNumbers.reduce((total, number) => total + number, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arrayNumbers) {
+  return arrayNumbers.reduce((total, number) => total * number);
 };
 
-const power = function() {
-	
+const power = function(numberA, numberB) {
+	return numberA ** numberB;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  return number == 0 ? 1 : factorial(number - 1) * number;
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
+const palindromes = function (rawString) {
+    const string = rawString.replace(/[ !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, '');
+    const reverseString = string.split("").reverse().join("");
 
+    return string.localeCompare(reverseString, undefined, {sensitivity: "base"}) === 0;
 };
 
 // Do not edit below this line
