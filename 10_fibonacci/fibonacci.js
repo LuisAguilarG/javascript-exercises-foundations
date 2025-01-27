367924499b6cf0cb2b22c0e5f0c7c6a6f68f5b82
Luis Aguilar <95900211+LuisAguilarG@users.noreply.github.com>
2025-01-27 03:03:15 -0600

    Finish '10', '11', and '12' exercises

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(totalTerms) {
+    let penultimateFibNumber = 0;
+    let lastFibNumber = 1;
+    let currentFibNumber = 0;
 
+    if (totalTerms < 0) {
+        return "OOPS";
+    }
+
+    for (let term = 1; term <= totalTerms; term++) {
+        if (term == 1) {
+            currentFibNumber = 1;
+        } else if (term > 1) {
+            currentFibNumber = penultimateFibNumber + lastFibNumber;
+            penultimateFibNumber = lastFibNumber;
+            lastFibNumber = currentFibNumber;
+        }
+    }
+    return currentFibNumber;
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function (peopleArray) {
+    return peopleArray.reduce((oldestPerson, currentPerson) => {
+        const ageOldestPerson = "yearOfDeath" in oldestPerson ? (oldestPerson.yearOfDeath - oldestPerson.yearOfBirth) : (new Date().getFullYear() - oldestPerson.yearOfBirth);
+        const ageCurrentPerson = "yearOfDeath" in currentPerson ? (currentPerson.yearOfDeath - currentPerson.yearOfBirth) : (new Date().getFullYear() - currentPerson.yearOfBirth);
+        
+        /* 
+        console.log("Comparison -------");
+        console.log(oldestPerson.name + ": " + ageOldestPerson);
+        console.log(currentPerson.name + ": " + ageCurrentPerson);
+         */
 
+        return ageOldestPerson > ageCurrentPerson ? oldestPerson : currentPerson;
+    });
 };
 
 // Do not edit below this line
